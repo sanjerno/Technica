792b24267c55b4a681826a2b64242a8a3a622f4c
santisnotboss <santiagocoreas@outlook.com>
2022-03-12 18:53:57 -0600

    Start of cobalt

--- src/main/java/net/technica/Technica.java
+++ src/main/java/net/technica/Technica.java
@@ -1,6 +1,8 @@
 package net.technica;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -8,11 +10,15 @@ import net.minecraft.util.registry.Registry;
 
 public class Technica implements ModInitializer {
 
-    public static final Item FABRIC_ITEM = new Wrench(new Item.Settings().group(ItemGroup.MISC).maxCount(1));
+    public static final Item WRENCH = new Wrench(new Item.Settings().group(ItemGroup.MISC).maxCount(1));
+
+    public static final Block COBALT_ORE = new cobalt_ore();
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("technica", "wrench"), FABRIC_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("technica", "wrench"), WRENCH);
+        Registry.register(Registry.BLOCK, new Identifier("technica", "cobalt_ore"), COBALT_ORE);
+        Registry.register(Registry.ITEM, new Identifier("technica", "cobalt_ore"), new BlockItem(COBALT_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
     
 }
